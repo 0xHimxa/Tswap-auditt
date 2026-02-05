9b3c6fef2af577da566e9865ca9719b67f6a9c3a
himxa <himxa0x@gmail.com>
2026-02-05 14:50:21 +0100

    added the basic functionality for invariant

---
+++ test/invariant/Invariant.t.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+import { Test, console } from "forge-std/Test.sol";
+import { PoolFactory } from "src/PoolFactory.sol";
+import { ERC20Mock } from "../mocks/Erc20.sol";
+import { TSwapPool } from "src/TSwapPool.sol";
+import {StdInvariant} from "forge-std/StdInvariant.sol";
+
+contract Invariant is StdInvariant, Test {
+//thses pools have 2 assets 
+ ERC20Mock poolToken;
+ ERC20Mock mockWeth;
+// we need a x and y balance
+
+int256 constant STARTING_X= 100e18; //Starting ERC20 /poolToken balance
+int256 constant STARTING_Y= 50e18; //Starting WETH 
+
+ //we re goona need the contracts
+ PoolFactory factory;
+ TSwapPool pool; //pooltoken/weth
+    function setUp () public{
+        mockWeth = new ERC20Mock();
+        poolToken = new ERC20Mock();
+        factory = new PoolFactory(address(mockWeth));
+        pool = new TSwapPool(factory.createPool(address(poolToken)));
+    
+      //create these initail x and y balances
+        poolToken.mint(address(pool), uint256(STARTING_X));
+        mockWeth.mint(address(pool), uint256(STARTING_Y));
+    }
+}
